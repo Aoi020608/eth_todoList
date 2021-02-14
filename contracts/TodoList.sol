7e8cf4315d19fda787a8c38fac43206c2442dca6
Aoi <ht.so.ik0102@gmail.com>
2021-02-14 18:33:31 +0700

    list tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,7 +4,22 @@ contract TodoList {
 
 	uint public taskCount = 0;
 
-	
+	struct Task {
+		uint id;
+		string content;
+		bool completed;
+	}
+
+	mapping(uint => Task) public tasks;
+
+	constructor() public {
+		createTask("Checkout dappuniversity.com");
+	}
+
+	function createTask(string memory _content) public {
+		taskCount ++;
+		tasks[taskCount] = Task(taskCount, _content, false);
+	} 
 
 
 
